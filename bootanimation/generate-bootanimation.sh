2a2fd0110fe46ef1aed41aeb3ce801d91570a84f
Frank Eggen <eggen@emden.net>
2018-05-22 05:13:39 +0200

    Revert "Setup bootanimation for androidbw"
    This reverts commit c077711139d021805d88aea2670547d9dbb6d297.

--- bootanimation/generate-bootanimation.sh
+++ bootanimation/generate-bootanimation.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-#WIDTH="$1"
-#HEIGHT="$2"
-#HALF_RES="$3"
+WIDTH="$1"
+HEIGHT="$2"
+HALF_RES="$3"
 OUT="$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION"
 
-#if [ "$HEIGHT" -lt "$WIDTH" ]; then
-#    IMAGEWIDTH="$HEIGHT"
-#else
-#    IMAGEWIDTH="$WIDTH"
-#fi
+if [ "$HEIGHT" -lt "$WIDTH" ]; then
+    IMAGEWIDTH="$HEIGHT"
+else
+    IMAGEWIDTH="$WIDTH"
+fi
 
-#IMAGESCALEWIDTH="$IMAGEWIDTH"
-#IMAGESCALEHEIGHT=$(expr $IMAGESCALEWIDTH / 3)
+IMAGESCALEWIDTH=1080
+IMAGESCALEHEIGHT=1920
 
-#if [ "$HALF_RES" = "true" ]; then
-#    IMAGEWIDTH=$(expr $IMAGEWIDTH / 2)
-#fi
+if [ "$HALF_RES" = "true" ]; then
+    IMAGEWIDTH=$(expr $IMAGEWIDTH / 2)
+fi
 
-#IMAGEHEIGHT=$(expr $IMAGEWIDTH / 3)
+IMAGEHEIGHT=$(expr $IMAGEWIDTH / 3)
 
-RESOLUTION="1080x1920"
+RESOLUTION=""$IMAGEWIDTH"x"$IMAGEHEIGHT""
 
 for part_cnt in 0 1 2 3 4
 do
